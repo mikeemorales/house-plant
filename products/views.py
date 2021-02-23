8f8a258dcfadfdcb89bb4905dcd5ccf5ebc6cd1e
mikeemorales <j.mikeemorales@gmail.com>
2021-02-23 06:29:23 +0000

    adjust changes in views and urls

--- products/views.py
+++ products/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
-# Create your views here.
-
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -22,7 +20,7 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': product,
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
