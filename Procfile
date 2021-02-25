d1a2af4b403664588b50ba6c0f04faf673e4b555
mikeemorales <j.mikeemorales@gmail.com>
2021-02-25 22:19:12 +0000

    deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn house_plant.wsgi:application
--- house_plant/settings.py
+++ house_plant/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 't431h-a7+@_%@54p@g$3&ljo+r&lt#1di)6@_k+^50*d*j$=vb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokuadmin-house-plant-online.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'house_plant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
+        }
 
 
 # Password validation
