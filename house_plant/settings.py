400cb10d341ad852ae62ab4f6752a764fc9585e3
mikeemorales <j.mikeemorales@gmail.com>
2021-02-26 01:35:53 +0000

    cache control

--- house_plant/settings.py
+++ house_plant/settings.py
@@ -177,6 +177,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 PST',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'house-plant-proj'
     AWS_S3_REGION_NAME = 'us-west-1'
